cee3e2270de14c7e5b7b63764a6cf9d2789d20b5
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-19 11:28:17 -0300

    Implementando o LogOut

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
 })
 export class ToolbarNavigationComponent {
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+  ){}
 
+  handleLogout(): void
+  {
+    this.cookieService.delete('jwt_token');
+    void this.router.navigate(['/home']);
+  }
 }
