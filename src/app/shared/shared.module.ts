89dd0dfa5ab091c2d2f8344926ab88429f77e4d8
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-03 07:45:53 -0300

    Implementando Pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,12 +7,14 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { DialogService } from 'primeng/dynamicdialog';
 import { ToolbarNavigationComponent } from './components/toolbar-navigation/toolbar-navigation.component';
+import { ShortenPipe } from './pipes/shorten/shorten.pipe';
 
 
 
 @NgModule({
   declarations: [
-    ToolbarNavigationComponent
+    ToolbarNavigationComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule,
@@ -25,6 +27,9 @@ import { ToolbarNavigationComponent } from './components/toolbar-navigation/tool
     ButtonModule
   ],
   providers: [DialogService, CurrencyPipe],
-  exports: [ToolbarNavigationComponent]
+  exports: [
+    ToolbarNavigationComponent,
+    ShortenPipe,
+  ]
 })
 export class SharedModule { }
